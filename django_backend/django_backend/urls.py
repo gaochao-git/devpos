2b068e54ce28cd4403dd3edcb75a127540ba4b46
1187982934@qq.com <1187982934@qq.com>
2020-02-14 12:18:46 +0800

    合并代码

--- django_backend/django_backend/urls.py
+++ django_backend/django_backend/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from apps.cluster import cluster
 from apps.sql import inception
+from apps.common import create_common_user
+from apps.common import migrate_common_user
+from apps.privilege import create_private_user
+from apps.server import cloud_instance
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -21,5 +26,18 @@ urlpatterns = [
     path('check_sql/', inception.check_sql_func),
     path('submit_sql/', inception.submit_sql_func),
     path('pass_submit_sql_by_uuid/', inception.pass_submit_sql_by_uuid_func),
+    path('get_cloud_info/', cloud_instance.get_cloud_instance_func),                                     # 查看云主机信息
+    path('get_user_info/', create_common_user.get_user_info_func),                                       # 公共账号管理
+    path('grant_user_info/', create_common_user.create_and_grant_func),                                  # 创建用户申请权限
+    path('migrate_common_user/', migrate_common_user.migrate_common_user_func),                          # 同步公共账号
+    path('get_private_user_info/', create_private_user.get_private_user_info_func),                      # 查看私有账号信息
+    path('privileges_extend_info/', create_private_user.privileges_extend_info_func),                    # 权限扩展
+    path('get_order_info/', create_private_user.get_order_info_func),                                    # 查看工单信息
+    path('privileges_create_user_info/', create_private_user.privileges_create_user_info_func),          # 新建用户工单提交
+    path('privileges_original_info/', create_private_user.privileges_original_info_func),                # 查看用户原始权限信息
+    path('check_order/', create_private_user.check_order_func),                                          # 审核工单
+    path('execute_order/', create_private_user.execute_order_func),                                      # 执行工单
+
+
 ]
 
