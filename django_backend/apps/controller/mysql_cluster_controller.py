d0b8e0cdc93464d53708934a1ae92b8d7ac17a03
gaochao <18853503922@163.com>
2020-05-17 20:40:53 +0800

    修改方法名

--- django_backend/apps/controller/mysql_cluster_controller.py
+++ django_backend/apps/controller/mysql_cluster_controller.py
@@ -12,13 +12,13 @@ logger = logging.getLogger('devops')
 
 
 # 获取所有集群信息
-def get_mysql_cluster_handler(request):
+def get_mysql_cluster_controller(request):
     ret = mysql_cluster.get_mysql_cluster()
     return HttpResponse(json.dumps(ret), content_type='application/json')
 
 
 # 模糊搜索集群名信息
-def get_mysql_cluster_by_cluster_name_handler(request):
+def get_mysql_cluster_by_cluster_name_controller(request):
     to_str = str(request.body, encoding="utf-8")
     cluster_name = json.loads(to_str)['cluster_name']
     ret = mysql_cluster.get_mysql_cluster_by_cluster_name(cluster_name)
