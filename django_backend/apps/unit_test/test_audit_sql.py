349c082671bb750cd0297644b2c8d98f71b5d38c
gaochao <18853503922@163.com>
2022-03-06 16:51:27 +0800

    feat(console):增加列显示功能

---
+++ django_backend/apps/unit_test/test_audit_sql.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+class viewTests(TestCase):
+    fixtures = ['db.json']
+    def test_case(self):
+        """
+        If no Articles exist, 
+        'latest_article_list' should be empty
+        """
+        print(11111)
