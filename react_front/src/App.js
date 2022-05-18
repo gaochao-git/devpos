e1f7c75c98308654b4b296bfaf96e63776be29e7
gaochao <18853503922@163.com>
2022-05-19 07:48:08 +0800

    feat(webconsole):微调页面

--- react_front/src/App.js
+++ react_front/src/App.js
@@ -81,58 +81,7 @@ class App extends Component {
             }
         ).catch(err => {message.error(err.message)})
     }
-//setCurrentTimeInterVal = () => {
-//     this.currentTimerId= setInterval(() => {this.getDateWeek()}, 1000);
-//}
-//getDateWeek = ()=>{
-//    this.getTodayDate();
-//    this.getTodayWeek()
-//}
-//getTodayDate = ()=> {
-//  var checkTime = function (i) {
-//    if (i < 10) {i = "0" + i}
-//    return i;
-//  }
-//  var date = new Date();
-//  var year = date.getFullYear().toString();
-//  var month = (date.getMonth()+1).toString();
-//  var day = date.getDate().toString();
-//  var hour =   checkTime(date.getHours().toString());
-//  var minute = checkTime(date.getMinutes().toString());
-//  var second = checkTime(date.getSeconds().toString());
-//  var now_time = year+'-'+month+'-'+day+' '+hour+':'+minute+':'+second;
-//  this.setState({current_time:now_time})
-//};
-//
-//getTodayWeek = ()=> {
-//    var tempDate = new Date();
-//    var days = tempDate.getDay();
-//    var week;
-//    switch(days) {
-//        case 1:
-//            week = '星期一';
-//            break;
-//        case 2:
-//            week = '星期二';
-//            break;
-//        case 3:
-//            week = '星期三';
-//            break;
-//        case 4:
-//            week = '星期四';
-//            break;
-//        case 5:
-//            week = '星期五';
-//            break;
-//        case 6:
-//            week = '星期六';
-//            break;
-//        case 0:
-//            week = '星期日';
-//            break;
-//    }
-//    this.setState({week_day:week})
-//};
+
     //更改服务、运维、管理标签
     handlerClick = e =>{
         this.setState({current_nav:e.key});
