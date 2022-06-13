ba336f01cb869239f9d31f583d4f74bf4ac71f73
gaochao <18853503922@163.com>
2022-06-13 21:25:55 +0800

    fix(common):增加密码加解密

--- django_backend/django_backend/auth_config.py
+++ django_backend/django_backend/auth_config.py
@@ -1,4 +1,5 @@
 import socket
+from apps.utils.enc_dec_pass import decrypt_data
 import logging
 logger = logging.getLogger('devops')
 
@@ -17,24 +18,24 @@ if ip in pro_host_list:
     django_database_host = '47.104.2.74'
     django_database_port = 3306
     django_database_user = 'gaochao'
-    django_database_pass = 'fffjjj'
+    django_database_pass = decrypt_data('cPYHTlgHr79ec5Y835tjXQFLgecrFLs/WGW+Wp4gJLZcDGDTWrn27UEFfoCso/Z8h9kNof34wiTMvV/vY6MH5iXSCPbZ423lvYaPrBRO7rgtcWsk0LqrdrRZx/gQblOQSUXDSv3S47O2fox+jK/IO/vGk0YfY7XQGCtQaK4nQzTV/4ijsHhi0j1xb5xs6ATrOy2OWIEQkLZ+QKQmhwWwuu5F3LbdS8QmJplmqI/5k7fdpRanbiZBstkABWMWUjtfSDmnAdddGq3vtVvaUxoIiAPnSZCpkstfpdOn9JDFYiYfIzB3C6fuvzJNkm1zD0mqTaC7xwexnnfXqc3PARcZIA==')
     inception_host = '47.104.2.74'
     inception_port = 6669
     inception_backup_host = '47.104.2.74'
     inception_backup_port = 3306
     inception_backup_user = 'gaochao'
-    inception_backup_pass = 'fffjjj'
+    inception_backup_pass = decrypt_data('cPYHTlgHr79ec5Y835tjXQFLgecrFLs/WGW+Wp4gJLZcDGDTWrn27UEFfoCso/Z8h9kNof34wiTMvV/vY6MH5iXSCPbZ423lvYaPrBRO7rgtcWsk0LqrdrRZx/gQblOQSUXDSv3S47O2fox+jK/IO/vGk0YfY7XQGCtQaK4nQzTV/4ijsHhi0j1xb5xs6ATrOy2OWIEQkLZ+QKQmhwWwuu5F3LbdS8QmJplmqI/5k7fdpRanbiZBstkABWMWUjtfSDmnAdddGq3vtVvaUxoIiAPnSZCpkstfpdOn9JDFYiYfIzB3C6fuvzJNkm1zD0mqTaC7xwexnnfXqc3PARcZIA==')
 else:
     django_database_host = '47.104.2.74'
     django_database_port = 3306
     django_database_user = 'gaochao'
-    django_database_pass = 'fffjjj'
+    django_database_pass = decrypt_data('cPYHTlgHr79ec5Y835tjXQFLgecrFLs/WGW+Wp4gJLZcDGDTWrn27UEFfoCso/Z8h9kNof34wiTMvV/vY6MH5iXSCPbZ423lvYaPrBRO7rgtcWsk0LqrdrRZx/gQblOQSUXDSv3S47O2fox+jK/IO/vGk0YfY7XQGCtQaK4nQzTV/4ijsHhi0j1xb5xs6ATrOy2OWIEQkLZ+QKQmhwWwuu5F3LbdS8QmJplmqI/5k7fdpRanbiZBstkABWMWUjtfSDmnAdddGq3vtVvaUxoIiAPnSZCpkstfpdOn9JDFYiYfIzB3C6fuvzJNkm1zD0mqTaC7xwexnnfXqc3PARcZIA==')
     inception_host = '47.104.2.74'
     inception_port = 6669
     inception_backup_host = '47.104.2.74'
     inception_backup_port = 3306
     inception_backup_user = 'gaochao'
-    inception_backup_pass = 'fffjjj'
+    inception_backup_pass = decrypt_data('cPYHTlgHr79ec5Y835tjXQFLgecrFLs/WGW+Wp4gJLZcDGDTWrn27UEFfoCso/Z8h9kNof34wiTMvV/vY6MH5iXSCPbZ423lvYaPrBRO7rgtcWsk0LqrdrRZx/gQblOQSUXDSv3S47O2fox+jK/IO/vGk0YfY7XQGCtQaK4nQzTV/4ijsHhi0j1xb5xs6ATrOy2OWIEQkLZ+QKQmhwWwuu5F3LbdS8QmJplmqI/5k7fdpRanbiZBstkABWMWUjtfSDmnAdddGq3vtVvaUxoIiAPnSZCpkstfpdOn9JDFYiYfIzB3C6fuvzJNkm1zD0mqTaC7xwexnnfXqc3PARcZIA==')
 
 CONNECT_INFO = {
     # 项目数据库配置
