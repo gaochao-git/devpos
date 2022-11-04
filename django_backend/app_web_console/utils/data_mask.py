6c4117686716ec7df00a07c46b48ce51a0dc3465
gaochao <18853503922@163.com>
2022-11-04 10:10:15 +0800

    feat(web_console):采用正则配置脱敏规则

--- django_backend/app_web_console/utils/data_mask.py
+++ django_backend/app_web_console/utils/data_mask.py
@@ -3,6 +3,7 @@
 # @Author  : 高超
 
 import json
+import re
 from utils.go_inception_ import MyGoInception
 
 import logging
@@ -11,17 +12,21 @@ logger = logging.getLogger("devops")
 
 def data_masking(instance_name, db_name, sql, sql_result):
     """
-    脱敏数据
-    sql = 'select "1111x",concat(user,"xx"),host,authentication_string from mysql.user'
-    mask_metadata_config = ['mysql.user.host', 'mysql.user.user']
-    sql_result =[{'1111x': '1111x','concat(user,"xx")':'999999', 'host': 'localhost','authentication_string': 'fffjjj'}]
+    采取异常捕获,脱敏失败不阻断查询，这个依据不同公司要就自己决定是阻断还是放行
+    脱敏数据案例
+      sql = 'select "1111x",concat(user,"xx"),host,authentication_string from mysql.user'
+      mask_metadata_config = ['mysql.user.host', 'mysql.user.user']
+      sql_result =[{'1111x': '1111x','concat(user,"xx")':'999999', 'host': 'localhost','authentication_string': 'fffjjj'}]
+    一些常用脱敏规则
+      .*\..*\..*: 所有库、所有表、所有列
+      test[00-32]*\.emp*\.name: test_00库到test_32库、emp开头的表、name列
     :param instance_name:数据库ip_port
     :param db_name:数据库名
     :param sql:执行的SQL
     :param sql_result:执行SQL对应的结果
     :return:[{'1111x': '1111x', 'concat(user,"xx")': '#########', 'host': '#########', 'authentication_string': 'fffjjj'}]
     """
-    mask_metadata_config = ['mysql.user.host', 'mysql.user.user']
+    mask_rule_config = ['test[00-32]*\.emp*\.name', '.*\..*\..*']  # 正则减少分库分表场景配置
     host, port = instance_name.split('_')[0], instance_name.split('_')[1]
     try:
         # 通过goInception获取select list
@@ -35,7 +40,7 @@ def data_masking(instance_name, db_name, sql, sql_result):
         hint_columns_position = []
         for field in select_field_position_map_list:
             for position, db_tb_col in field.items():
-                if db_tb_col.lower() in mask_metadata_config:
+                if any(re.match(r"%s" % rule, db_tb_col.lower(), re.I) for rule in mask_rule_config):
                     hint_columns_position.append(position)
         # 脱敏列数据,直接在原始结果中进行脱敏
         for hint_column in hint_columns_position:
